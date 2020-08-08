f764a3f274604e5c9bcc876f0ac237f829899cb1
unknown <marcospsw96@gmail.com>
2020-08-08 11:38:16 -0300

    Ajustes de Pastas e Rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Route } from 'react-router-dom';
-import Landing from './pages/landing';
+import Landing from './pages/Landing';
 import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
 
